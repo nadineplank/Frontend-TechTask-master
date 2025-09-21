3081cddfd685af0bfca4bb938fd77d2ecead122a
Nadine <nadine@contentpass.de>
2025-09-21 14:08:59 +0200

    Add Layout component

---
+++ client/src/components/Layout/Layout.tsx
@@ -0,0 +1,58 @@
+import React, { ReactNode } from "react";
+import styled from "@emotion/styled";
+import { Header } from "../Header/Header";
+import { Sidebar } from "../Sidebar/Sidebar";
+import { Footer } from "../Footer/Footer";
+import { Category } from "../../types";
+import { theme } from "../../styles/theme";
+
+const PageContainer = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: 200px 1fr;
+  grid-template-areas:
+    "header header"
+    "sidebar content"
+    "footer footer";
+  margin: 6px;
+  min-height: 100vh;
+  background-color: ${theme.colors.background.primary};
+
+  & > * {
+    padding: 10px;
+  }
+
+  @media (max-width: ${theme.breakpoints.mobile}) {
+    grid-template-columns: 1fr;
+    grid-template-areas:
+      "header"
+      "sidebar"
+      "content"
+      "footer";
+  }
+`;
+
+const Content = styled.main`
+  grid-area: content;
+  padding: ${theme.spacing.lg};
+`;
+
+interface LayoutProps {
+  children: ReactNode;
+  categories?: Category[];
+  isLoading?: boolean;
+}
+
+export const Layout: React.FC<LayoutProps> = ({
+  children,
+  categories = [],
+}) => {
+  return (
+    <PageContainer>
+      <Header />
+      <Sidebar categories={categories} />
+      <Content>{children}</Content>
+      <Footer />
+    </PageContainer>
+  );
+};
