b35aa7239b0b06c301e5d08b23a229f1c1ff7e40
Nadine <nadine@contentpass.de>
2025-09-21 17:27:52 +0200

    Move test file

--- client/src/pages/ProductListPage.test.tsx
+++ client/src/pages/ProductListPage.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import ProductListPage from "./pages/ProductListPage";
+import ProductListPage from "./ProductListPage";
 
 test("renders the ProductList", () => {
   const { getByText } = render(<ProductListPage />);
